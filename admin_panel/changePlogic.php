e937c1f4b15f825e919448c034dffeb3b24ac8fa
soulsoft <soulsoftinfotech@gmail.com>
2023-02-27 01:07:16 -0800

    bgh

--- admin_panel/changePlogic.php
+++ admin_panel/changePlogic.php
@@ -2,9 +2,11 @@
 include('../config.php');
 
 // $email = $_GET['aEmail'];
-$adminpassword = $_GET['changeP'];
+$adminpassword = $_POST['changeP'];
+// echo $a
 // echo $password;
-$encypassAdmin = md5($password);
+$encypassAdmin = md5($adminpassword);
+// $encypassAdmin = $adminpassword;
 
 $declinenewusers = "UPDATE `admin_login` SET `aPassword`='$encypassAdmin' WHERE `admin_id`='2'";
 $result = mysqli_query($con, $declinenewusers);
@@ -13,8 +15,8 @@ $result = mysqli_query($con, $declinenewusers);
 if ($result) {
     echo "<script>
     alert('Your password change');
-    window.location.href='http://localhost/soulsoftin/adminSign.html';
     </script>";
+    // window.location.href='http://localhost/soulsoftin/adminSign.html';
 } else {
     echo "Something error";
 }
